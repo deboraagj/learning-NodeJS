3ba107e39a7af8475d9b55311fd2a7631f17da4e
c-fonseca <deboraaragao2004@gmail.com>
2025-10-27 21:23:19 -0300

    nodejs

---
+++ ExpressRoutes/app.js
@@ -0,0 +1,37 @@
+/*
+
+para ser possível a utilização da biblioteca express é necessário instalar a mesma na máquina local através do
+comando "npm install express --save"
+
+para desintalar alguma biblioteca usa-se o comando "npm uninstall nomedabiblioteca"
+
+*/
+
+const express = require('express'); 
+const app = express();
+
+// o primeiro argumento é a porta que vai ser ouvida
+// o segundo argumento é para retornar o sucesso
+
+// ROTAS COM NODEJS
+
+app.get("/index", function(req, res){
+    res.sendFile(__dirname+"/html/index.html");
+});
+
+app.get("/about", function(req, res){
+    res.sendFile(__dirname+"/html/about.html");
+});
+
+app.get("/ola/:name", function(req, res){
+    res.send(`<h1> Olá meu caro(a) ${req.params.name} </h1>`);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, function(){
+    console.log("Server runnig in url http://localhost:"+PORT);
+});
+
+// biblioteca para atualizar o servidor o servidor automaticamente: nodemon. Comando para instalar " npm install nodemon"
+
